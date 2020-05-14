c8843fdf6facb427317b02b25a1a8dbd38690eb6
Dmitro Yagolnik <dima_yagolnik@ukr.net>
2020-05-14 14:42:12 +0300

    Updates

---
+++ precache-manifest.1b59e2e4c13284b80944970f2c686445.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a3d7473773bd2fd5268301c2163990",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "c81c10621ec7a0826fce",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.d77b72bc.chunk.css"
+  },
+  {
+    "revision": "c9eaad826352a7a28f79",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.4596521d.chunk.js"
+  },
+  {
+    "revision": "c81c10621ec7a0826fce",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.699defb8.chunk.js"
+  },
+  {
+    "revision": "ccd86a36cebbc0e1304f",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.6b5e3725.js"
+  }
+]);
